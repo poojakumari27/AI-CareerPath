a21ac1dcb640cad348816c2e9af8f53ccdc27891
poojakumari27 <poojakumari27ee@gmail.com>
2025-07-04 17:14:59 +0530

    ai

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,25 @@
-import { serve } from "inngest/next";
+export async function GET(req) {
+  const { serve } = await import("inngest/next");
+  const { inngest } = await import("@/lib/inngest/client");
+  const { generateIndustryInsights } = await import("@/lib/inngest/function");
 
-import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/function";
+  const { GET } = serve({
+    client: inngest,
+    functions: [generateIndustryInsights],
+  });
 
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [generateIndustryInsights],
-});
+  return GET(req);
+}
+
+export async function POST(req) {
+  const { serve } = await import("inngest/next");
+  const { inngest } = await import("@/lib/inngest/client");
+  const { generateIndustryInsights } = await import("@/lib/inngest/function");
+
+  const { POST } = serve({
+    client: inngest,
+    functions: [generateIndustryInsights],
+  });
+
+  return POST(req);
+}
